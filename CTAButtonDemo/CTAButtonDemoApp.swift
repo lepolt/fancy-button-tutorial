e8cf47781f9cb1641239ca48de1d232bbf7d4709
Jonathan Lepolt <lepolt@gmail.com>
2023-05-18 14:12:39 -0400

    Initial Commit

---
+++ CTAButtonDemo/CTAButtonDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CTAButtonDemoApp.swift
+//  CTAButtonDemo
+//
+//  Created by Jonathan Lepolt on 5/18/23.
+//
+
+import SwiftUI
+
+@main
+struct CTAButtonDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
